bdb31332206bfb6595219044c0db9c358147befb
danpeacock <danpeacock96@gmail.com>
2022-11-02 17:36:22 +0000

    add name

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "vsphere_datacenter" "datacenter" {
   name = var.dc_name
 }
 data "vsphere_host" "host" {
-  name          = "ESXI-HASH01.homelab.local"
+  name          = "192.168.116.90"
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
