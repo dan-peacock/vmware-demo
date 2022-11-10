7239c96b58b991e14942929592187acc998435bf
danpeacock <danpeacock96@gmail.com>
2022-11-10 14:49:06 +0000

    azure changes

--- main.tf
+++ main.tf
@@ -7,45 +7,124 @@ terraform {
   }
 }
 
-provider "vsphere" { 
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
-  vsphere_server       = var.vsphere_server
-  allow_unverified_ssl = true
+provider "azurerm" {
+  features {}
+  subscription_id   = var.subscription_ID
+  tenant_id         = var.tenant_ID
+  client_id         = var.SP_AppID
+  client_secret     = var.SP_Password
 }
 
-data "vsphere_datacenter" "datacenter" {
-  name = var.dc_name
+#Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
 }
 
-data "vsphere_datastore" "datastore" {
-  name          = "datastore1 (1)"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
-}
-data "vsphere_compute_cluster" "cluster" {
-  name          = "Henlow"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
-}
-
-data "vsphere_network" "network" {
-  name          = "VM Network"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
+#Create the virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  name             = "AELTC-TF"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 1
-  guest_id         = "other3xLinux64Guest"
-  wait_for_guest_net_timeout = 0
-  # wait_for_guest_net_routable = false
-  memory           = 1024
-  network_interface {
-    network_id = data.vsphere_network.network.id
+#Create internal subnet
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
   }
-    disk {
-    label = "aeltc-tf-disk"
-    size  = 5
+}
+
+#Specify VM options
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = var.vm_size
+  #Delete the disks automatically when deleting the VM
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = var.default_password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+      environment = var.tags
+      
   }
 }
+
+# provider "vsphere" { 
+#   user                 = var.vsphere_user
+#   password             = var.vsphere_password
+#   vsphere_server       = var.vsphere_server
+#   allow_unverified_ssl = true
+# }
+
+# data "vsphere_datacenter" "datacenter" {
+#   name = var.dc_name
+# }
+
+# data "vsphere_datastore" "datastore" {
+#   name          = "datastore1 (1)"
+#   datacenter_id = data.vsphere_datacenter.datacenter.id
+# }
+# data "vsphere_compute_cluster" "cluster" {
+#   name          = "Henlow"
+#   datacenter_id = data.vsphere_datacenter.datacenter.id
+# }
+
+# data "vsphere_network" "network" {
+#   name          = "VM Network"
+#   datacenter_id = data.vsphere_datacenter.datacenter.id
+# }
+
+# resource "vsphere_virtual_machine" "vm" {
+#   name             = "AELTC-TF"
+#   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+#   datastore_id     = data.vsphere_datastore.datastore.id
+#   num_cpus         = 1
+#   guest_id         = "other3xLinux64Guest"
+#   wait_for_guest_net_timeout = 0
+#   # wait_for_guest_net_routable = false
+#   memory           = 1024
+#   network_interface {
+#     network_id = data.vsphere_network.network.id
+#   }
+#     disk {
+#     label = "aeltc-tf-disk"
+#     size  = 5
+#   }
+# }
